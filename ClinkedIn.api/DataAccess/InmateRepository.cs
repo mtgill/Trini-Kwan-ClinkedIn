e59d92e063185d80d6e53179b2e6f4ddeac09fa3
Emily DeWitt <47046126+emilykdewitt@users.noreply.github.com>
2019-09-30 20:22:28 -0500

    Added inmate model, seed data, and add method (in InmateRepository)

--- ClinkedIn.api/DataAccess/InmateRepository.cs
+++ ClinkedIn.api/DataAccess/InmateRepository.cs
@@ -7,5 +7,48 @@ namespace ClinkedIn.Api.DataAccess
 {
     public class InmateRepository
     {
+        static List<Inmate> _inmates = new List<Inmate>
+        {
+            new Inmate
+            {
+                Id = Guid.NewGuid(),
+                Name = "Nathan",
+                Location = "Atlanta",
+                CriminalInterest = CriminalInterest.Murder,
+                ReleaseDate = new DateTime(2030, 04, 20),
+            },
+            new Inmate
+            {
+                Id = Guid.NewGuid(),
+                Name = "Adam",
+                Location = "Los Angeles",
+                CriminalInterest = CriminalInterest.Robbery,
+                ReleaseDate = new DateTime(2025, 06, 15),
+            },
+            new Inmate
+            {
+                Id = Guid.NewGuid(),
+                Name = "Martin",
+                Location = "New York",
+                CriminalInterest = CriminalInterest.Assault,
+                ReleaseDate = new DateTime(2050, 03, 05),
+            },
+            new Inmate
+            {
+                Id = Guid.NewGuid(),
+                Name = "Greg",
+                Location = "Boston",
+                CriminalInterest = CriminalInterest.Bribery,
+                ReleaseDate = new DateTime(2022, 08, 22),
+            },
+
+        };
+
+        public Inmate Add(Inmate newInmate)
+        {
+            _inmates.Add(newInmate);
+
+            return newInmate;
+        }
     }
 }
