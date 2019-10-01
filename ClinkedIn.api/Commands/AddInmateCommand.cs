bd731b828a76d606e8c95e2cdaa4fa8a4bf94cc0
Emily DeWitt <47046126+emilykdewitt@users.noreply.github.com>
2019-09-30 20:32:08 -0500

    Added enum for CriminalInterest

---
+++ ClinkedIn.api/Commands/AddInmateCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public class AddInmateCommand
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string CriminalInterest { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
