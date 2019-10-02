157f28ef91f12641e5f76c97a11cf823938d4162
Matt Gill <mtgill90@outlook.com>
2019-10-01 20:05:13 -0500

    pulled master

--- ClinkedIn.api/Controllers/InmatesController.cs
+++ ClinkedIn.api/Controllers/InmatesController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ClinkedIn.Api.Commands;
-using ClinkedIn.Api.DataAccess;
-using ClinkedIn.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using ClinkedIn.Api.Models;
+using ClinkedIn.Api.DataAccess;
+using ClinkedIn.Api.Commands;
 
 namespace ClinkedIn.Api.Controllers
 {
@@ -13,33 +13,6 @@ namespace ClinkedIn.Api.Controllers
     [ApiController]
     public class InmateController : Controller
     {
-            [HttpGet]
-            public ActionResult<IEnumerable<Inmate>> GetAllInmates()
-            {
-                var repo = new InmateRepository();
-
-                return repo.GetAll();
-            }
-
-        [HttpPost]
-        public IActionResult CreateInmate(AddInmateCommand newAddInmateCommand)
-        {
-            var newInmate = new Inmate
-            {
-                Id = Guid.NewGuid(),
-                Name = newAddInmateCommand.Name,
-                Location = newAddInmateCommand.Location,
-                CriminalInterest = newAddInmateCommand.CriminalInterest,
-                ReleaseDate = newAddInmateCommand.ReleaseDate,
-            };
-
-            var repo = new InmateRepository();
-
-            var inmateThatGotCreated = repo.Add(newInmate);
-
-            return Created($"api/inmates/{inmateThatGotCreated.Name}", inmateThatGotCreated);
-        }
-
         // Returns all friends for a given inmate
         [HttpGet("{inmateName}/allFriends")]
         public ActionResult<IEnumerable<string>> GetFriendsByName(string inmateName)
@@ -50,7 +23,7 @@ namespace ClinkedIn.Api.Controllers
         }
 
         // Start of AddFriend 
-        [HttpPost("{name}/friends")]
+        [HttpPost("{name}/friends")] 
         public IActionResult AddNewFriend(AddFriendCommand friendToAdd, string name)
         {
             var repo = new InmateRepository();
